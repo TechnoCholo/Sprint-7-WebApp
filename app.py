e3bafc20f2c74c320e3f13ac26cd0146b3e77cd1
Andres Montfort <andy117@live.com>
2026-07-26 14:00:05 -0600

    Fix dataset merge conflict

--- app.py
+++ app.py
@@ -29,4 +29,13 @@ if disp_button: # al hacer clic en el botón
      
          # mostrar un gráfico Plotly 
      st.plotly_chart(scatter_fig, use_container_width=True)
-     
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+    histogram_fig = px.histogram(car_data, x="odometer")
+         
+             # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(histogram_fig, use_container_width=True)
